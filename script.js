38ebe4b811a19afdb9671fc192ce2eb35b3fa9cb
JuliaSmyshnikova <smyshnikova1@gmail.com>
2022-12-26 18:53:27 +0100

    move projekt

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-alert("hi");
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Tuersday",
+  "Friday",
+  "Sarurday",
+  "Sunday",
+];
+let InfoDay = document.querySelector("#info-day");
+let day = days[now.getDay()];
+let hour = now.getHours();
+let minute = now.getMinutes();
+InfoDay.innerHTML = `${day} ,${hour}:${minute}`;
+
+function Search(event) {
+  event.preventDefault();
+  let Search = document.querySelector("#input-city");
+  let city = document.querySelector("#city");
+  city.innerHTML = Search.value;
+}
+
+let form = document.querySelector("#form-serch");
+form.addEventListener("submit", Search);
+
+function convertFa(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = -8 + "°";
+}
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", convertFa);
+
+function convertCe(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = 16 + "°";
+}
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", convertCe);
